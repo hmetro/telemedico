089fe099e22904dac8fa9e7aec0fea3f9311c955
mchang@hmetro.med.ec <admin@beta.cloud.hm.med.ec>
2020-07-02 16:52:33 -0500

    Definido templates y controllers para Modulo Configuracion o Administracion

--- app/controllers/configuracionController.php
+++ app/controllers/configuracionController.php
@@ -13,6 +13,7 @@ namespace app\controllers;
 
 use Ocrend\Kernel\Controllers\Controllers;
 use Ocrend\Kernel\Controllers\IControllers;
+use Ocrend\Kernel\Helpers as Helper;
 use Ocrend\Kernel\Router\IRouter;
 
 /**
@@ -31,7 +32,53 @@ class configuracionController extends Controllers implements IControllers
             'users_logged' => true,
         ));
 
-        $this->template->display('configuracion/' . $this->user['name_rol'] . '/configuracion');
+        global $config;
+
+        // Para Controladores
+        if ($router->getController() == 'configuracion' && is_null($router->getMethod()) && is_null($router->getId())) {
+            $this->template->display('configuracion/roles/' . $this->user['rol'] . '/configuracion', array(
+                'appBodyClass'  => 'page-profile',
+                'configuracion' => 'active',
+            ));
+
+        }
+
+        // Para metodos
+        if (!is_null($router->getMethod()) && is_null($router->getId())) {
+            switch ($router->getMethod()) {
+                case 'calendario':
+                    $this->template->display('calendario/roles/' . $this->user['rol'] . '/calendario', array(
+                        'appBodyClass' => 'app-calendar',
+                        'teleconsulta' => 'active',
+                        'calendario'   => '#0168fa',
+                    ));
+                    break;
+                case 'pacientes':
+                    $this->template->display('pacientes/roles/' . $this->user['rol'] . '/pacientes', array(
+                        'appBodyClass' => 'app-contact',
+                        'teleconsulta' => 'active',
+                        'pacientes'    => '#0168fa',
+                    ));
+                    break;
+                default:
+                    Helper\Functions::redir($config['build']['url'] . 'configuracion');
+                    break;
+            }
+
+        }
+
+        // Para metodos y ids
+        if (!is_null($router->getMethod()) && !is_null($router->getId())) {
+            switch ($router->getId()) {
+                case 'zoom':
+                    echo "zoom";
+                    break;
+                default:
+                    Helper\Functions::redir($config['build']['url'] . 'configuracion');
+                    break;
+            }
+
+        }
 
     }
 }
